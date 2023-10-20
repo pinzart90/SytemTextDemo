848818c6ecdb90eafa18896467e58f6e96ef570e
pinzart <tiberiu.pinzariu@autodesk.com>
2023-10-20 12:02:41 -0400

    add project

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+namespace Demo
+{
+  class Program
+  {
+    public static void Main(string[] _)
+    {
+      //AppDomain.CurrentDomain.AssemblyResolve += ResolveAssembly;
+      var obj = new System.Text.Json.JsonDocumentOptions();
+      System.Console.WriteLine(obj);
+    }
+
+    public static Assembly ResolveAssembly(object sender, ResolveEventArgs args)
+    {
+      var assemblyName = new AssemblyName(args.Name).Name + ".dll";
+      System.Console.WriteLine(assemblyName);
+      return null;
+    }
+  }
+}
